8e3826bd55614bc91ebfdc6f3eb6769e20aa8a4d
veronica <astilvero6@gmail.com>
2021-12-10 22:21:15 -0500

    Agregar archivos de proyecto.

---
+++ JuegoBebe/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace JuegoBebe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PanelJuego = new System.Windows.Forms.Panel();
+            this.btnReinicio = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblRecord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PanelJuego
+            // 
+            this.PanelJuego.Location = new System.Drawing.Point(104, 70);
+            this.PanelJuego.Name = "PanelJuego";
+            this.PanelJuego.Size = new System.Drawing.Size(960, 572);
+            this.PanelJuego.TabIndex = 0;
+            // 
+            // btnReinicio
+            // 
+            this.btnReinicio.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btnReinicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReinicio.ForeColor = System.Drawing.Color.LightGray;
+            this.btnReinicio.Location = new System.Drawing.Point(1126, 139);
+            this.btnReinicio.Name = "btnReinicio";
+            this.btnReinicio.Size = new System.Drawing.Size(147, 79);
+            this.btnReinicio.TabIndex = 1;
+            this.btnReinicio.Text = "Reiniciar juego";
+            this.btnReinicio.UseVisualStyleBackColor = false;
+            this.btnReinicio.Click += new System.EventHandler(this.btnReinicio_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(1136, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Record: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblRecord
+            // 
+            this.lblRecord.AutoSize = true;
+            this.lblRecord.ForeColor = System.Drawing.Color.Transparent;
+            this.lblRecord.Location = new System.Drawing.Point(1211, 168);
+            this.lblRecord.Name = "lblRecord";
+            this.lblRecord.Size = new System.Drawing.Size(13, 13);
+            this.lblRecord.TabIndex = 3;
+            this.lblRecord.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(1271, 541);
+            this.Controls.Add(this.btnReinicio);
+            this.Controls.Add(this.lblRecord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PanelJuego);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Baby Shower";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelJuego;
+        private System.Windows.Forms.Button btnReinicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblRecord;
+    }
+}
+
---
+++ JuegoBebe/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JuegoBebe
+{
+    public partial class Form1 : Form
+    {
+        int TamanioColumnasFilas = 4;
+        int Movimientos = 0;
+        int CantidadCartasVolteadas = 0;
+        List<string> CartasEnumeradas;
+        List<string> CartasRevueltas;
+        ArrayList CartasSeleccionadas;
+        PictureBox CartaT1;
+        PictureBox CartaT2;
+        int CartaActual=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            iniciarJuego();
+        }
+
+        public void iniciarJuego()
+        {
+            timer1.Enabled = false;
+            timer1.Stop();
+            lblRecord.Text = "0";
+            CantidadCartasVolteadas = 0;
+            Movimientos = 0;
+            PanelJuego.Controls.Clear();
+            CartasEnumeradas = new List<string>();
+            CartasRevueltas = new List<string>();
+            CartasSeleccionadas = new ArrayList();
+            for (int i = 0; i < 8; i++)
+            {
+                CartasEnumeradas.Add(i.ToString());
+                CartasEnumeradas.Add(i.ToString());
+            }
+            var Aleatorio = new Random();
+            var Resultado = CartasEnumeradas.OrderBy(item => Aleatorio.Next());
+            foreach(string ValorCarta in Resultado)
+            {
+                CartasRevueltas.Add(ValorCarta);
+            }
+            var tablaPanel = new TableLayoutPanel();
+            tablaPanel.RowCount = TamanioColumnasFilas;
+            tablaPanel.ColumnCount = TamanioColumnasFilas;
+            for (int i = 0; i < TamanioColumnasFilas; i++)
+            {
+                var Porcentaje = 150f / (float)TamanioColumnasFilas - 10;
+                tablaPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent,Porcentaje));
+                tablaPanel.RowStyles.Add(new RowStyle(SizeType.Percent,Porcentaje));
+            }
+            int contadorFichas = 1;
+
+            for (int i = 0; i < TamanioColumnasFilas; i++)
+            {
+                for (int j = 0; j < TamanioColumnasFilas; j++)
+                {
+                    var CartasJuego = new PictureBox();
+                    CartasJuego.Name = string.Format("{0}", contadorFichas);
+                    CartasJuego.Dock = DockStyle.Fill;
+                    CartasJuego.SizeMode = PictureBoxSizeMode.StretchImage;
+                    CartasJuego.Image = Properties.Resources.Isham;
+                    CartasJuego.Cursor = Cursors.Hand;
+                    CartasJuego.Click += btnCarta_Click;
+                    tablaPanel.Controls.Add(CartasJuego, j, i);
+                    contadorFichas++;
+                }
+            }
+            tablaPanel.Dock = DockStyle.Fill;
+            PanelJuego.Controls.Add(tablaPanel);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReinicio_Click(object sender, EventArgs e)
+        {
+            iniciarJuego();
+        }
+        private void btnCarta_Click(object sender, EventArgs e)
+        {
+            if (CartasSeleccionadas.Count < 2)
+            {            
+            Movimientos++;
+            lblRecord.Text = Convert.ToString(Movimientos);
+            var CartaSeleccionadaUsuario = (PictureBox)sender;
+
+            CartaActual = Convert.ToInt32(CartasRevueltas[Convert.ToInt32(CartaSeleccionadaUsuario.Name) - 1]);
+            CartaSeleccionadaUsuario.Image = RecuperarImagen(CartaActual);
+            CartasSeleccionadas.Add(CartaSeleccionadaUsuario);
+
+                if (CartasSeleccionadas.Count==2)
+                {
+                    CartaT1 = (PictureBox)CartasSeleccionadas[0];
+                    CartaT2 = (PictureBox)CartasSeleccionadas[1];
+                    int Carta1 = Convert.ToInt32(CartasRevueltas[Convert.ToInt32(CartaT1.Name)-1]);
+                    int Carta2 = Convert.ToInt32(CartasRevueltas[Convert.ToInt32(CartaT2.Name) - 1]);
+                    
+                    if(Carta1 != Carta2)
+                    {
+                        timer1.Enabled = true;
+                        timer1.Start();;
+                    } else
+                    {
+                        CantidadCartasVolteadas++;
+                        if (CantidadCartasVolteadas>7)
+                        {
+                            MessageBox.Show("El juego ha terminado");
+                        }
+                        CartaT1.Enabled = false; CartaT2.Enabled = false;
+                        CartasSeleccionadas.Clear();
+                    }
+                }
+            }
+        }
+        public Bitmap RecuperarImagen(int NumeroImagen)
+        {
+            Bitmap TmpImg = new Bitmap(200, 100);
+            switch (NumeroImagen)
+            {
+                case 0: TmpImg = Properties.Resources.img8;
+                    break;
+                default: TmpImg = (Bitmap)Properties.Resources.ResourceManager.GetObject("img" + NumeroImagen);
+                    break;
+            }
+            return TmpImg;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int TiempoVirarCarta = 1;
+            if (TiempoVirarCarta == 1)
+            {
+                CartaT1.Image=Properties.Resources.Isham;
+                CartaT2.Image = Properties.Resources.Isham;
+                CartasSeleccionadas.Clear();
+                TiempoVirarCarta = 0;
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
